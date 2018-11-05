b233a251efafe843d336fff031e5181c7e418818
Near321 <35989617+Near321@users.noreply.github.com>
2018-11-04 19:18:53 -0800

    Update MySecondJavaOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MySecondJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MySecondJavaOpMode.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -19,6 +21,8 @@ public class MySecondJavaOpMode extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
+    private DcMotor arm = null;
+    private CRServo capturing = null;
 
     DcMotor left;
     DcMotor right;
@@ -34,6 +38,8 @@ public class MySecondJavaOpMode extends LinearOpMode {
         telemetry.update();
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        arm = hardwareMap.get(DcMotor.class,"arm");
+        capturing = hardwareMap.get(CRServo.class,"capturing");
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
         // Wait for the game to start (driver presses PLAY)
@@ -42,14 +48,29 @@ public class MySecondJavaOpMode extends LinearOpMode {
 
         while (opModeIsActive()) {
 
+
             double leftPower;
             double rightPower;
-            double drive = -gamepad1.left_stick_y;
-            double turn  = gamepad1.right_stick_x;
+            //change the negative
+            boolean button_press = gamepad1.a;
+            boolean reverse_button_press = gamepad1.b;
+            double drive = gamepad1.left_stick_y;
+            double turn  = -gamepad1.right_stick_x;
             leftPower = Range.clip(drive + turn, -1.0, 1.0) ;
             rightPower = Range.clip(drive - turn, -1.0, 1.0) ;
+            capturing.setPower(200);
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
+            if (button_press == true) {
+                arm.setPower(1000);
+                sleep(250);
+                arm.setPower(0);
+            }
+            if (reverse_button_press == true) {
+                arm.setPower(-1000);
+                sleep(250);
+                arm.setPower(0);
+            }
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
